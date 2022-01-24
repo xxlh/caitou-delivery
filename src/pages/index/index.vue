3b0e9751030179c5dbf7c13cd8938a7b857f21fd
Esone <01@eexx.me>
2022-01-24 13:23:44 +0800

    app tabs

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,61 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
+    <image class="logo" src="/static/logo.png" @tap="aa" />
+    <navigator url="/pages/my/my" class="text-area">
       <text class="title">{{ title }}</text>
-    </view>
+    </navigator>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello2')
+function aa() {
+  alert(11)
+}
+
+// #ifdef APP-PLUS
+const wxp_Amap = uni.requireNativePlugin("WXP-Amap");
+// 插件内置方法使用
+wxp_Amap.permission(); // 用于申请定位权限，
+// 持续定位
+wxp_Amap.start({  // {} 大括号内参数说明见下方
+    setInterval: 5000, // 定位间隔，单位 ms，不是必须的，默认 2000ms
+    cacheEnable: true // 是否允许缓存，默认为true，既当位置不变时返回最后一次定位的地址，不是必须的
+}, result => {
+    // 处理回调结果
+    // result 为回调结果 见下方 start() 回调结果
+    console.log(JSON.stringify(result));
+})
+// #endif
+
+// #ifndef H5
+let time = setInterval(function() {  
+    uni.getLocation({  
+        type: 'gcj02 ',  
+        success: function(res) {  
+            // uni.setStorageSync('userLocation', JSON.stringify(res));  
+            console.log(11, res)
+            uni.showToast({  
+                title: JSON.stringify(res),  
+                icon: 'none'  
+            }); 
+        },  
+        fail: function() {  
+          console.log(22);
+          
+            uni.showToast({  
+                title: '获取位置失败',  
+                icon: 'none'  
+            });  
+        },
+        complete: (res) => {
+          console.log(33, res);
+          
+        }
+    });  
+}, 3000);  
+// #endif
 </script>
 
 <style>
